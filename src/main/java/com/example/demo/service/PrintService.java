04cc6cd02b634cd7f915fef39f2b4a3c72f88554
dan.marc <dan.marceddu@gmail.com>
2026-05-02 00:28:35 +0200

    aggiornato print

--- src/main/java/com/example/demo/service/PrintService.java
+++ src/main/java/com/example/demo/service/PrintService.java
@@ -22,19 +22,20 @@ public class PrintService {
             log.info("Print disabled (print.enabled=false)");
             return;
         }
-		for (int i = 0; i < 2; i++) {
+
 			try (Socket socket = new Socket("192.168.1.10", 9100); OutputStream out = socket.getOutputStream()) {
 				// modifica doppia stampa
-
+				for (int i = 0; i < 2; i++) {
 				out.write(new byte[] { 0x1B, 0x40 }); // init
 				out.write(receiptText.getBytes(StandardCharsets.US_ASCII));
 				out.write("\n\n\n".getBytes(StandardCharsets.US_ASCII));
 				out.write(new byte[] { 0x1D, 0x56, 0x41, 0x10 }); // cut
+				}
 				out.flush();
 
 			} catch (Exception e) {
 				log.error("Print error", e);
 			}
-		}
+
     }
 }
